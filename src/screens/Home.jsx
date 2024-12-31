40386b738edf6f8fa970732d7c2a2c39c0a74081
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-12-31 07:02:54 -0300

    I added the Radio Tk section to the home.

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { StyleSheet, ScrollView } from "react-native";
 import Header from "../components/header/Header";
-import LightGreenCard from "../components/llightGreenCard/LightGreenCard ";
+import LightGreenCard from "../components/llightGreenCard/LightGreenCard";
 import colorPalette from "../helpers/color_palette";
 import SelectorMenu from "../components/selectorMenu/SelectorMenu";
 import LastPoems from "../components/lastPoems/LastPoems";
 import SevenLastPoems from "../components/lastPoems/SevenLastPoems";
+import LastYoutubeVideo from "../components/lastYoutubeVideo/LastYoutubeVideo";
+
 function Home() {
   return (
     <ScrollView style={styles.container}>
       <Header />
       <LightGreenCard />
-      <SelectorMenu/>
-      <LastPoems/>
-      <SevenLastPoems/>
+      <SelectorMenu />
+      <LastPoems /> 
+       <LastYoutubeVideo videoId="tTfMid0Sweg" /> 
+      <SevenLastPoems />
+     
     </ScrollView>
   );
 }
@@ -23,6 +27,6 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colorPalette.neutralLight
-  }
+    backgroundColor: colorPalette.neutralLight,
+  },
 });
