c7be57a571e5fe49a7bd828e0f6bc29b5f81e4c3
JuanLaspiur <laspiurperezjuan@gmail.com>
2025-01-22 20:00:09 -0300

    Guardado

--- src/components/optionsMyProfile/MyFollowers.jsx
+++ src/components/optionsMyProfile/MyFollowers.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import colorPalette from '../../helpers/color_palette';
+import OtherUserCard from './components/OtherUserCard';
 
 const MyFollowers = () => {
   return (
-    <View>
-      <Text>Mis Seguidores</Text>
+    <View style={styles.container}>
+        <Text style={styles.title} >Mis Seguidores</Text>
+        <OtherUserCard/>
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+    container: {
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+    }, 
+    title:{
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: colorPalette.accent,
+        paddingTop:15,   
+         padding:10
+      
+    },
+    pagination: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop: 20,
+    },
+    pageText: {
+      marginHorizontal: 10,
+      fontSize: 16,
+      color:colorPalette.accent,
+    },
+  });
 export default MyFollowers;
---
+++ src/components/optionsMyProfile/components/OtherUserCard.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import colorPalette from '../../../helpers/color_palette';
+
+function OtherUserCard({ user }) {
+  return (
+    <TouchableOpacity>
+    <View style={styles.card}>
+      <Image source={{ uri: 'https://this-person-does-not-exist.com/img/avatar-gence1a1e33fb6a8bb54cea76516040b661.jpg' }} style={styles.avatar} />
+      <Text style={styles.username}>Harcode</Text>
+    </View></TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+    card: {
+      backgroundColor: 'rgba(255, 255, 255, 0.7)',
+      flexDirection: 'row', 
+      alignItems: 'center',
+      padding: 15,
+      borderRadius: 8,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 6 }, 
+      shadowOpacity: 0.2, 
+      shadowRadius: 10,
+      marginBottom: 20,
+      width: '100%',
+    },
+    avatar: {
+      width: 50,
+      height: 50,
+      borderRadius: 15,
+      marginRight: 15,
+    },
+    username: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      color: '#333',
+      color:colorPalette.accent
+    },
+  });
+
+
+export default OtherUserCard;
