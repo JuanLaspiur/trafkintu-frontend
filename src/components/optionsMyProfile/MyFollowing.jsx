a781b815486b97e164b0f984acec37d619a6c89b
JuanLaspiur <laspiurperezjuan@gmail.com>
2025-01-23 10:25:25 -0300

    Guardar seguidores y seguidos

--- src/components/optionsMyProfile/MyFollowing.jsx
+++ src/components/optionsMyProfile/MyFollowing.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import colorPalette from '../../helpers/color_palette';
+import OtherUserCard from './components/OtherUserCard';
 
 const MyFollowing = () => {
   return (
-    <View>
-      <Text>Mis Seguidos</Text>
-    </View>
+    <View style={styles.container}>
+    <Text style={styles.title} >Mis Seguidos</Text>
+    <OtherUserCard/>
+</View>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  }, 
+  title:{
+      fontSize: 16,
+      fontWeight: 'bold',
+      color: colorPalette.accent,
+      paddingTop:15,   
+       padding:10
+    
+  },
+  pagination: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  pageText: {
+    marginHorizontal: 10,
+    fontSize: 16,
+    color:colorPalette.accent,
+  },
+});
 export default MyFollowing;
