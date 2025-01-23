4b151405a959f16c1038353f4ad8d8b722ce7216
JuanLaspiur <laspiurperezjuan@gmail.com>
2025-01-23 17:31:29 -0300

    Subir escrito terminado

--- src/screens/UploadPoem.jsx
+++ src/screens/UploadPoem.jsx
@@ -2,15 +2,16 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView, Alert } from 'react-native';
 import colorPalette from '../helpers/color_palette';
 import ImagesModal from '../components/imagesModal/ImagesModal'; 
-import { useNavigation } from '@react-navigation/native';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { Image } from 'expo-image';
+import { useNavigation } from '@react-navigation/native';
 
 function UploadPoem() {
   const [titulo, setTitulo] = useState('');
   const [contenido, setContenido] = useState('');
   const [imagen, setImagen] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
-  const navigation = useNavigation();
+   const navigation = useNavigation();
 
   const manejarEnvio = () => {
     if (!imagen) {
@@ -23,10 +24,6 @@ function UploadPoem() {
     setTitulo('');
     setContenido('');
     setImagen(null);
-  };
-
-  const manejarBorrador = () => {
-    Alert.alert('Borrador guardado', 'Tu escrito ha sido guardado como borrador');
   };
 
   const abrirModalImagenes = () => {
@@ -44,28 +41,25 @@ function UploadPoem() {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.header}>
       <TouchableOpacity 
-          style={styles.backButton} 
           onPress={() => navigation.goBack()} 
         >
-          <AntDesign name="left" size={27} color='black' />
+          <AntDesign name="left" size={27} color={colorPalette.accent} />
         </TouchableOpacity>
-
+      </View>
       <TouchableOpacity onPress={abrirModalImagenes} style={styles.imageWrapper}>
         <Text style={styles.imagePreview}>
-          {imagen ? 'Imagen seleccionada' : 'Toca para subir una imagen'}
+          {imagen ? 'Imagen seleccionada' : 'Selecciona una imagen'}
         </Text>
       </TouchableOpacity>
-
-      <Text style={styles.header}>Subir Escrito</Text>
-
+      <Text style={styles.headerText}>Subir Escrito</Text>
       <TextInput
         style={styles.input}
         placeholder="Título del escrito"
         value={titulo}
         onChangeText={setTitulo}
       />
-
       <TextInput
         style={[styles.input, styles.textarea]}
         placeholder="Escribe tu escrito aquí..."
@@ -74,17 +68,9 @@ function UploadPoem() {
         multiline
         numberOfLines={6}
       />
-
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={[styles.button, styles.submitButton]} onPress={manejarEnvio}>
-          <Text style={styles.buttonText}>Subir </Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={[styles.button, styles.draftButton]} onPress={manejarBorrador}>
-          <Text style={styles.buttonText}>Borrador</Text>
-        </TouchableOpacity>
-      </View>
-
+      <TouchableOpacity style={[styles.button, styles.submitButton]} onPress={manejarEnvio}>
+        <Text style={styles.buttonText}>Subir Escrito</Text>
+      </TouchableOpacity>
       <ImagesModal
         visible={modalVisible}
         onClose={cerrarModal}
@@ -101,17 +87,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexGrow: 1,
   },
-  header: {
+  header:{
+    height:70,
+    width:'100%',
+    justifyContent:'center',
+    paddingTop:10
+  },
+  headerText: {
     fontSize: 24,
     fontWeight: '600',
     color: colorPalette.primary,
     marginBottom: 20,
-  },
-  backButton: {
-    position: 'absolute',
-    top: 70,
-    left: 18,
-    zIndex: 1,
   },
   input: {
     width: '100%',
@@ -126,24 +112,14 @@ const styles = StyleSheet.create({
     height: 250,
     textAlignVertical: 'top',
   },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-  },
   button: {
     paddingVertical: 12,
     paddingHorizontal: 20,
     borderRadius: 8,
     marginVertical: 8,
-    flex: 1,
-    marginRight: 8,
   },
   submitButton: {
     backgroundColor: colorPalette.primary,
-  },
-  draftButton: {
-    backgroundColor: '#D3D3D3', 
   },
   buttonText: {
     fontSize: 16,
@@ -152,8 +128,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   imageWrapper: {
-    marginVertical: 16, 
-    marginTop: 100,
+    marginVertical: 16,
     padding: 10,
     backgroundColor: '#ccc',
     borderRadius: 8,
