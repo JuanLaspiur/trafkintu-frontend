645681093c3835cb3f66378c68ae41d5643b6021
JuanLaspiur <laspiurperezjuan@gmail.com>
2025-01-29 11:30:42 -0300

    Guardar

--- src/components/authorInfo/AuthorInfo.jsx
+++ src/components/authorInfo/AuthorInfo.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useAuth } from '../../contexts/AuthContext';
 import { Image } from 'expo-image';
 import colorPalette from '../../helpers/color_palette';
 
-function AuthorInfo({ name, avatar }) {
-   const navigation = useNavigation();
+function AuthorInfo({ id = '1',name, avatar }) {
+  const { user } =useAuth(); 
+  const navigation = useNavigation();
+
+  const goToProfile = ()=>{
+    if(user._id == id) {
+      navigation.navigate("Profile", { name, avatar })
+      }
+      else {
+        navigation.navigate("OtherUserProfile", { name, avatar })
+      }
+  }
+
   return (
-    <TouchableOpacity   onPress={() => navigation.navigate("Profile", { name, avatar })} style={styles.authorContainer}>
-   
+    <TouchableOpacity   onPress={goToProfile} style={styles.authorContainer}>
       <Image source={{ uri: avatar }} style={styles.authorAvatar} />
       <Text style={styles.authorName}>{name}</Text> 
     </TouchableOpacity>
