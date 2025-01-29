ee8ef958647a5512366a969704cf853e6c0beb45
JuanLaspiur <laspiurperezjuan@gmail.com>
2025-01-29 19:54:02 -0300

    Boton ver más 7 ultios

--- src/components/lastPoems/SevenLastPoems.jsx
+++ src/components/lastPoems/SevenLastPoems.jsx
@@ -6,7 +6,7 @@ import { useNavigation } from "@react-navigation/native";
 import colorPalette from "../../helpers/color_palette";
 import { formatDateToDDMMYYYY } from "../../helpers/formatDate";
 
-function SevenLastPoems({lastSevenPoems}) {
+function SevenLastPoems({ lastSevenPoems }) {
   const poems = lastSevenPoems;
 
   const [showAll, setShowAll] = useState(false);
@@ -17,30 +17,36 @@ function SevenLastPoems({lastSevenPoems}) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Siete últimos más leídos:</Text>
+      <Text style={styles.title}>Siete últimos:</Text>
       <View style={styles.cardsContainer}>
         {visiblePoems.map((poem) => (
           <MiniPoemCard
+            key={poem._id} // Añadir una clave única para cada tarjeta
             title={poem.title}
             author={poem.author}
             date={formatDateToDDMMYYYY(poem.createdAt)}
             image={poem.image}
-            image_route= "../../../assets/gift/yellow_kiss.webp"
+            image_route="../../../assets/gift/yellow_kiss.webp"
+            content={poem.content}
           />
         ))}
+        {/* Mostrar "Ver todos" solo cuando showAll sea true */}
+        {showAll && (
+          <MiniCardSeeAll onPress={togglePoems} />
+        )}
       </View>
       <TouchableOpacity style={styles.showMoreButton} onPress={togglePoems}>
         <Ionicons
           name={showAll ? "chevron-up" : "chevron-down"}
           size={30}
-          color={colorPalette.secondary}
+          color={colorPalette.acent} // Usamos colorPalette.acent
         />
       </TouchableOpacity>
     </View>
   );
 }
 
-function MiniPoemCard({ title, author, date, image, image_route }) {
+function MiniPoemCard({ title, author, date, image, image_route, content }) {
   const navigation = useNavigation();
 
   const handleCardPress = () => {
@@ -49,11 +55,11 @@ function MiniPoemCard({ title, author, date, image, image_route }) {
       image,
       author,
       date,
-      image_route
+      image_route,
+      content,
     });
   };
 
-  // Ensure author and date are strings
   const authorString = author ? (typeof author === 'string' ? author : author.name) : '';
   const dateString = date ? (typeof date === 'string' ? date : formatDateToDDMMYYYY(date)) : '';
 
@@ -66,17 +72,28 @@ function MiniPoemCard({ title, author, date, image, image_route }) {
       <Image source={image} style={styles.miniImage} />
       <Text style={styles.miniCardTitle}>{title}</Text>
       <Text style={styles.miniCardSubtitle}>
-        <Image source={require('../../../assets/icons/autor_card.webp')} style={styles.iconTitleCard} /> {authorString} • {dateString}
+        <Image
+          source={require('../../../assets/icons/autor_card.webp')}
+          style={styles.iconTitleCard}
+        />{" "}
+        {authorString} • {dateString}
       </Text>
     </TouchableOpacity>
   );
 }
 
+function MiniCardSeeAll({ onPress }) {
+  return (
+    <TouchableOpacity onPress={onPress} activeOpacity={0.7} style={styles.button}>
+      <Text style={styles.buttonText}>Ver todos</Text>
+    </TouchableOpacity>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20
+    padding: 20,
   },
   title: {
     fontSize: 17,
@@ -85,16 +102,17 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     textShadowColor: colorPalette.neutralDark,
     textShadowOffset: { width: 1, height: 1 },
-    textShadowRadius: 3
+    textShadowRadius: 3,
   },
   cardsContainer: {
     flexDirection: "row",
     flexWrap: "wrap",
     justifyContent: "space-between",
-    gap: 10
-  },  iconTitleCard:{
-    height:12,
-    width:12,
+    gap: 10,
+  },
+  iconTitleCard: {
+    height: 12,
+    width: 12,
   },
   miniCard: {
     width: "45%",
@@ -106,11 +124,11 @@ const styles = StyleSheet.create({
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
-    shadowRadius: 4
+    shadowRadius: 4,
   },
   miniImage: {
     width: "100%",
-    height: 80
+    height: 80,
   },
   miniCardTitle: {
     fontSize: 14,
@@ -118,21 +136,43 @@ const styles = StyleSheet.create({
     color: "#fff",
     paddingHorizontal: 10,
     paddingTop: 5,
-    marginBottom:-2
+    marginBottom: -2,
   },
   miniCardSubtitle: {
     fontSize: 12,
     fontWeight: "400",
     color: "#fff",
     paddingHorizontal: 10,
-    paddingBottom: 5
+    paddingBottom: 5,
   },
   showMoreButton: {
     marginTop: 15,
     alignItems: "center",
     marginHorizontal: "20%",
-    padding: 10
-  }
+    padding: 10,
+  },
+  button: {
+    width: "45%",
+    backgroundColor: colorPalette.accent,
+    borderRadius: 10,
+    overflow: "hidden",
+    marginBottom: 10,
+    elevation: 3,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: "#fff",
+    marginRight: 8,
+  },
 });
 
 export default SevenLastPoems;
+
